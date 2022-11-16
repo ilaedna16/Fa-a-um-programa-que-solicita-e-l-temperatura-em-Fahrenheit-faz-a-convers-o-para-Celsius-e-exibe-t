411983bbd14ac93326026028fbc0f47b3870131f
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 16:35:24 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float c, f; 
+    
+    printf ("digite a temperatura em graus fahreneit\n");
+    scanf ("%f",&f); 
+    
+    c= ((f-32) / 1.8);
+    
+    printf ("a temperatura em graus celsius eh:%f\n", c); 
+    
+    
+    
+    return 0;
+}
